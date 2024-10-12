8dae3a210700f34f095f90d2c0c212705a809dfb
Vunny-max <farhanfajar222@gmail.com>
2024-10-12 11:35:08 +0700

    Add files via upload

---
+++ kalkulator.js
@@ -0,0 +1,20 @@
+function tambah() {
+    let box1 = document.getElementById("box1").value
+    let box2 = document.getElementById("box2").value
+    console.log(Number(box1) + Number(box2))
+}
+function kurang() {
+    let box1 = document.getElementById("box1").value
+    let box2 = document.getElementById("box2").value
+    console.log(Number(box1) - Number(box2))
+}
+function kali() {
+    let box1 = document.getElementById("box1").value
+    let box2 = document.getElementById("box2").value
+    console.log(Number(box1) * Number(box2))
+}
+function bagi() {
+    let box1 = document.getElementById("box1").value
+    let box2 = document.getElementById("box2").value
+    console.log(Number(box1) / Number(box2))
+}
